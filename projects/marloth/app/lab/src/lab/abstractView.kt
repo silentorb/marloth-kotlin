04f3965285d360d84f98f2ce4aa859e8e8f511ef
cj <christopherwjohnson@gmail.com>
2018-01-08 09:17:44 -0700

    Added a grid to the world view.

--- projects/marloth/app/lab/src/lab/abstractView.kt
+++ projects/marloth/app/lab/src/lab/abstractView.kt
@@ -5,13 +5,15 @@ import generation.Node
 import generation.WorldBoundary
 import mythic.bloom.Bounds
 import mythic.drawing.Canvas
+import mythic.spatial.Vector2
 import mythic.spatial.Vector4
 import mythic.spatial.times
 import org.joml.minus
 import org.joml.plus
 import org.joml.xy
 
-private val worldPadding = 20f
+private val worldPadding = 20f // In screen units
+private val gridSpacing = 10f // In world units
 
 fun getScale(bounds: Bounds, worldBoundary: WorldBoundary): Float {
   val worldDimensions = worldBoundary.dimensions
@@ -26,12 +28,36 @@ fun getScale(bounds: Bounds, worldBoundary: WorldBoundary): Float {
   }
 }
 
+fun drawGrid(canvas: Canvas, bounds: Bounds, worldBoundary: WorldBoundary, scale: Float) {
+  val offset = bounds.position + worldPadding
+  val steps = (worldBoundary.dimensions.x / gridSpacing).toInt() - 1
+  val gridColor = Vector4(0f, 0f, 0f, 0.3f)
+  val gridGap = gridSpacing * scale
+  val gridBrush = canvas.solid(gridColor)
+  val length = worldBoundary.dimensions.x * scale
+  for (i in 0..steps) {
+    canvas.drawSquare(
+        Vector2(0f, i * gridGap) + offset,
+        Vector2(length, 1f),
+        gridBrush
+    )
+
+    canvas.drawSquare(
+        Vector2(i * gridGap, 0f) + offset,
+        Vector2(1f, length),
+        gridBrush
+    )
+  }
+}
+
 fun drawAbstractWorld(bounds: Bounds, canvas: Canvas, world: AbstractWorld) {
-  val solid = canvas.solid(Vector4(1f, 1f, 0f, 1f))
-  val outline = canvas.outline(Vector4(1f, 0f, 0f, 1f), 5f)
+  val solid = canvas.solid(Vector4(0.7f, 0.6f, 0.5f, 0.6f))
+  val outline = canvas.outline(Vector4(0.3f, 0f, 0f, 0.8f), 3f)
   val scale = getScale(bounds, world.boundary)
   val offset = bounds.position + worldPadding
   fun getPosition(node: Node) = offset + (node.position.xy - world.boundary.start.xy) * scale
+
+  drawGrid(canvas, bounds, world.boundary, scale)
 
   for (node in world.nodes) {
     val radius = node.radius * scale
@@ -45,8 +71,8 @@ fun drawAbstractWorld(bounds: Bounds, canvas: Canvas, world: AbstractWorld) {
   }
 
   canvas.drawSquare(
-      bounds.position + worldPadding,
-      bounds.dimensions - worldPadding * 2,
+      offset,
+      world.boundary.dimensions.xy * scale,
       canvas.outline(Vector4(0.6f, 0.5f, 0.5f, 0.5f), 3f)
   )
 }
