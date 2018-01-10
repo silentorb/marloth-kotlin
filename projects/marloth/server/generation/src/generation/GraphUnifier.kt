ff153d5e921c08513f0e733068be3f77dc22d95f
cj <christopherwjohnson@gmail.com>
2018-01-09 17:28:19 -0700

    Further fleshing out of the unifying code.  Making it more functional.

--- projects/marloth/server/generation/src/generation/GraphUnifier.kt
+++ projects/marloth/server/generation/src/generation/GraphUnifier.kt
@@ -1,40 +1,61 @@
 package generation
 
+typealias NodeGroup = MutableList<Node>
+
 fun getNodeDistance(first: Node, second: Node): Float =
     Math.max(0f, first.position.distance(second.position) - first.radius - second.radius)
 
-fun findNearestNode(node: Node, world: AbstractWorld): Node? {
-  if (world.nodes.size < 2)
-    return null
+data class NearestNodeResult(val node: Node, val distance: Float)
 
-  return world.nodes.asSequence()
-      .filter { it !== node }
-      .sortedBy { getNodeDistance(node, it) }
-      .firstOrNull()
+fun getNeighborsByDistance(node: Node, nodes: Sequence<Node>) = nodes.asSequence()
+    .filter { it !== node }
+    .map { NearestNodeResult(it, getNodeDistance(node, it)) }
+    .sortedBy { it.distance }
+
+fun findNearestNode(node: Node, nodes: Sequence<Node>) =
+    getNeighborsByDistance(node, nodes)
+        .firstOrNull()
+
+data class NodeGap(val first: Node, val second: Node, val distance: Float)
+
+fun findNearestGap(node: Node, nodes: Sequence<Node>): NodeGap? {
+  val result = findNearestNode(node, nodes)
+  if (result != null)
+    return NodeGap(node, result.node, result.distance)
+
+  return null
 }
 
-class GraphUnifier(val world: AbstractWorld) {
-  val unifiedNodes: MutableList<Node> = mutableListOf()
-  val changed: MutableList<Node> = mutableListOf()
+fun findShortestGap(firstGroup: Sequence<Node>, secondGroup: Sequence<Node>): NodeGap? =
+    firstGroup.map { findNearestGap(it, secondGroup) }
+        .filterNotNull()
+        .sortedBy { it.distance }
+        .firstOrNull()
 
-  private fun scanNode(node: Node): Boolean {
-    var changed = false
-    for (other in node.getNeighbors()) {
-      if (!unifiedNodes.contains(other)) {
-        unifiedNodes.add(other)
-        changed = true
-      }
+fun getNeighborsToAdd(node: Node, group: NodeGroup): Sequence<Node> =
+    node.getNeighbors().filter { !group.contains(it) }
+
+fun scanChanged(changed: List<Node>, group: NodeGroup) =
+    changed.asSequence()
+        .map { getNeighborsToAdd(it, group) }
+        .flatten()
+        .distinct()
+
+fun unifyWorld(world: AbstractWorld) {
+  if (world.nodes.size < 2)
+    return
+
+  val first = world.nodes.first()
+  val mainGroup = mutableListOf(first)
+  val groups: MutableList<NodeGroup> = mutableListOf()
+  groups.add(mainGroup)
+  var changed = listOf(first)
+
+  while (groups.size != 1 || mainGroup.size != world.nodes.size) {
+    changed = scanChanged(changed, mainGroup).toList()
+    if (changed.isEmpty()) {
+
+      throw Error("Could not finish processing nodes")
     }
-    return changed
-  }
-
-  fun unify() {
-    if (world.nodes.size < 2)
-      return
-
-    scanNode(world.nodes.first())
-//    while (unifiedNodes.size != world.nodes.size) {
-//
-//    }
   }
 }
