1749529b10ae024a931cb22ca3095f6544fd259b
cj <cj@silentorb.com>
2018-10-14 11:41:23 -0600

    Moved the simulation files deeper in the file structure to add test files.

---
+++ projects/marloth/server/simulation/src/test/SimulationSpec.kt
@@ -0,0 +1,18 @@
+import junit.framework.TestCase.*
+import mythic.spatial.Vector2
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
+
+class SimulationSpec : Spek({
+  describe("simulation logic") {
+
+    on("bodies collide with room corners") {
+
+      it("should smoothly settle in the corner") {
+
+      }
+    }
+  }
+})
