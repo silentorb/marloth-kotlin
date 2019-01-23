5cc705f5b06f398d324bb2ec612e50ca50849bc8
cj <christopherwjohnson@gmail.com>
2019-01-23 11:51:37 -0700

    Consolidated the ent utility functions into the Utility file.

--- projects/mythic/misc/ent/src/main/kotlin/mythic/ent/Entity.kt
+++ projects/mythic/misc/ent/src/main/kotlin/mythic/ent/Entity.kt
@@ -16,34 +16,3 @@ fun newIdSource(initialValue: Id): IdSource {
   var nextId: Id = initialValue
   return { nextId++ }
 }
-
-fun <T> pipe(initial: T, steps: List<(T) -> T>): T =
-    steps.fold(initial) { a, b -> b(a) }
-
-fun <T> pipe2(initial: T, vararg steps: (T) -> T): T =
-    steps.fold(initial) { a, b -> b(a) }
-
-fun <T> pipe(vararg steps: (T) -> T): (T) -> T = {
-  steps.fold(it) { a, b -> b(a) }
-}
-
-fun <T> pipe(steps: List<(T) -> T>): (T) -> T = {
-  steps.fold(it) { a, b -> b(a) }
-}
-
-fun <T> replace(collection: Collection<T>, condition: (T) -> Boolean, transform: (T) -> T) =
-    collection.map { item ->
-      if (condition(item))
-        transform(item)
-      else
-        item
-    }
-
-
-fun <T> replaceIndex(collection: Collection<T>, index: Int, newValue: T) =
-    collection.mapIndexed { i, item ->
-      if (i == index)
-        newValue
-      else
-        item
-    }
