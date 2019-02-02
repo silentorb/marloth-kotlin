461718bd1af50a03d888ae3a75ab8d6fd65d2f51
cj <cj@silentorb.com>
2019-02-02 08:38:06 -0700

    Experimenting more with metaballs but still haven't figured it out.

--- projects/metahub/imaging/src/main/kotlin/silentorb/raymarching/SampleScene.kt
+++ projects/metahub/imaging/src/main/kotlin/silentorb/raymarching/SampleScene.kt
@@ -3,10 +3,11 @@ package silentorb.raymarching
 import mythic.spatial.Vector3
 
 fun sampleScene(): Sdf =
-    blend(0.2f,
-        sphereSdf(Vector3(-0.6f, 0f, 0f), 0.8f),
-        sphereSdf(Vector3(0.6f, 0f, 0f), 0.8f),
-        sphereSdf(Vector3(0.7f, -0.4f, 0.6f), 0.4f)
+    blend(0.4f,
+        sphereSdf(Vector3(-0.7f, 0f, 0f), 0.6f),
+        sphereSdf(Vector3(0.7f, 0f, 0f), 0.6f)
+//        ,
+//        sphereSdf(Vector3(0.7f, -0.4f, 0.6f), 0.4f)
     )
 //    blend(0.3f,
 //        sphereSdf(Vector3(-0.5f, 0f, 0f), 1f),
--- projects/metahub/imaging/src/main/kotlin/silentorb/raymarching/Sdf.kt
+++ projects/metahub/imaging/src/main/kotlin/silentorb/raymarching/Sdf.kt
@@ -12,6 +12,13 @@ fun sphereSdf(center: Vector3, radius: Float): Sdf = { point ->
   PointDistance(point.distance(center) - radius)
 }
 
+fun diff(a: Float, b: Float): Float =
+    Math.max(a, -b)
+
+fun diff(a: Sdf, b: Sdf): Sdf = { point ->
+  PointDistance(diff(a(point).value, b(point).value))
+}
+
 fun plus(vararg items: Sdf): Sdf = { point ->
   items.map { it(point) }
       .firstSortedBy { it.value }
@@ -31,18 +38,18 @@ fun blend(range: Float, vararg items: Sdf): Sdf = { point ->
     val scaledRange = range + curved
     val j = a + b - scaledRange
     val c = listOf(a, j).sorted().first()
+    val h = a + b - range * 2f
     if (point.x == 0f && point.z == 0f) {
 //      if (point.x > -0.01f && point.x < 0.01f && point.z > -0.01f && point.z < 0.01f) {
-      println("${point.z} $a, $j, $gap")
+//      println("${point.z} $a, $j, $gap")
+      println("${point.z} $h")
     }
     if (isRayHit(j)) {
       val k = 0
     }
-    PointDistance(c)
+//    println("${point.z} $h")
+//    println(h)
+    PointDistance(diff(h, a))
   } else
     sorted.first()
-}
-
-fun diff(a: Sdf, b: Sdf): Sdf = { point ->
-  PointDistance(Math.max(a(point).value, b(point).value))
 }
