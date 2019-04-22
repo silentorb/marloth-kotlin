1706edefa14e122f88c26a0204738f506c350139
cj <cj@silentorb.com>
2019-04-22 16:19:33 -0600

    Now filtering interactive gui to objects roughly in front of the player.

--- projects/marloth/app/marloth_game/src/main/kotlin/marloth/integration/PrepareHud.kt
+++ projects/marloth/app/marloth_game/src/main/kotlin/marloth/integration/PrepareHud.kt
@@ -2,12 +2,19 @@ package marloth.integration
 
 import marloth.clienting.gui.HudData
 import mythic.ent.Id
+import mythic.spatial.Vector3
 import simulation.Interactable
 import simulation.World
 
 fun getVisibleInteractable(world: World, player: Id): Interactable? {
-  return world.deck.interactables.filter {
-    true
+  val playerBody = world.deck.bodies[player]!!
+  val playerCharacter = world.deck.characters[player]!!
+  val f = playerCharacter.facingVector
+  val facingVector = Vector3(f.x, f.y, 0f)
+  return world.deck.interactables.filter { (id, _) ->
+    val body = world.deck.bodies[id]!!
+    body.position.distance(playerBody.position) < 4
+        && (body.position - playerBody.position).normalize().dot(facingVector) > 0.8f
   }.values.firstOrNull()
 }
 
@@ -18,6 +25,6 @@ fun gatherHudData(world: World): HudData {
   return HudData(
       health = character.health,
       sanity = character.sanity,
-interactable = getVisibleInteractable(world, player)
-      )
+      interactable = getVisibleInteractable(world, player)
+  )
 }
